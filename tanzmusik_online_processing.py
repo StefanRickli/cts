7fbbc6fc98a6518ba9a1d7c15f5e7209074e0d2e
Stefan Rickli <stefanrickli@gmx.ch>
2018-07-29 13:30:55 +0200

    Add functions 'get_charts_difference' and 'write_result'

--- tanzmusik_online_processing.py
+++ tanzmusik_online_processing.py
@@ -4,19 +4,12 @@ from tanzmusik_online_get_charts import get_chartnumbers
 import time
 from pathlib import Path
 import pickle
+import console_utils
+import logging
 
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 base_path = 'chart_html/'
-
-base_from_year = 2008
-base_from_week = 20
-base_to_year = 2010
-base_to_week = 53
-
-compare_from_year = 2011
-compare_from_week = 1
-compare_to_year = 2018
-compare_to_week = 30
 
 
 def get_song_title(song_div):
@@ -78,11 +71,13 @@ def compile_set(from_year, from_week, to_year, to_week):
                 ind = (ind + 1) % 5
                 n_items_left = n_items - i - 1
                 time_left = n_items_left * sum(ts) / min(i + 1, 10)
-                print('{:03}/{:03}: Added {} to set. ~{:.0f}s left.'.format(i + 1, n_items, chartnumber, time_left))
+                print('\r{:03}/{:03}: Added {} to set. ~{:.0f}s left.'.format(i + 1, n_items, chartnumber, time_left), end='')
         except:
             pass
+    print()
 
     return compilation
+
 
 def chart_difference(base_from_year, base_from_week, base_to_year, base_to_week, compare_from_year, compare_from_week, compare_to_year, compare_to_week):
     print('Preparing base set.')
@@ -96,14 +91,28 @@ def chart_difference(base_from_year, base_from_week, base_to_year, base_to_week,
         print('Comparison set is empty.')
 
     # Remove all elements from the comparison-set that have already been seen in the base-set
-    return compare_set - base_set
-    
+    result = compare_set - base_set
+    print('There are {} new songs in total.'.format(len(result)))
+    return result
 
-if __name__ == '__main__':
 
-    new_songs_by_artist = list(chart_difference(base_from_year, base_from_week, base_to_year, base_to_week, compare_from_year, compare_from_week, compare_to_year, compare_to_week))
-    new_songs_by_artist = sorted(new_songs_by_artist, key=lambda x : x[0])
+def get_charts_difference(base_from_year, base_from_week, base_to_year, base_to_week, compare_from_year, compare_from_week, compare_to_year, compare_to_week, sort_by='artist'):
     
+    new_songs = list(chart_difference(base_from_year, base_from_week, base_to_year, base_to_week, compare_from_year, compare_from_week, compare_to_year, compare_to_week))
+
+    if sort_by == 'artist':
+        return sorted(new_songs, key=lambda x : x[0])
+
+    if sort_by == 'title':
+        return sorted(new_songs, key=lambda x : x[1])
+
+    if sort_by == 'dance_style':
+        return sorted(new_songs, key=lambda x : x[2])
+
+    print('Done.')
+
+
+def write_result(result):
     with open('new_songs.txt', 'w', encoding='utf-8') as f:
         f.write('Comparing the song charts in the period ({}-{}/{}-{}) to the song charts in the period ({}-{}/{}-{}) reveals the following new, unseen songs:'.format(base_from_year, base_from_week, base_to_year, base_to_week, compare_from_year, compare_from_week, compare_to_year, compare_to_week))
         for i, song in enumerate(new_songs_by_artist):
@@ -114,5 +123,22 @@ if __name__ == '__main__':
         for i, song in enumerate(new_songs_by_artist):
             f.write('{}\t{}\t{}\t{}\n'.format(song[0], song[1], song[2], song[3]))
 
-    print('Done.')
+    return result
+
+
+if __name__ == '__main__':
+    base_from_year = 2008
+    base_from_week = 20
+    base_to_year = 2010
+    base_to_week = 53
+
+    compare_from_year = 2011
+    compare_from_week = 1
+    compare_to_year = 2018
+    compare_to_week = 30
+
+    new_songs_by_artist = get_charts_difference(base_from_year, base_from_week, base_to_year, base_to_week, compare_from_year, compare_from_week, compare_to_year, compare_to_week)
+    
+    write_result(new_songs_by_artist)
+
 
