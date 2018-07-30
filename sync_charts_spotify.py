61dbfb202613d5425a29792b58e45ae75294af5b
Stefan Rickli <stefanrickli@gmx.ch>
2018-07-30 16:58:34 +0200

    Add tempo tapping capability

--- sync_charts_spotify.py
+++ sync_charts_spotify.py
@@ -4,6 +4,7 @@ from asciimatics.screen import Screen
 from time import sleep
 from trimmable_string import TrimmableString
 from spotipy_interface import sp_login, sp_find, sp_play, sp_pause_resume, sp_skip, sp_seek
+import tap_tempo
 
 def sleep_ms(t):
     sleep(t/1000)
@@ -33,11 +34,36 @@ new_songs = tmo_processing.get_charts_difference(base_from_year,
 def get_key_blocking(screen):
     no_input = True
     while no_input:
-        ev = screen.get_key()
-        if ev is not None:
+        key_code = screen.get_key()
+        if key_code is not None:
             no_input = False
         sleep_ms(1)
-    return ev
+    return key_code
+
+
+def init_tpm():
+    return tap_tempo.TPMTapper()
+
+
+def update_tpm(screen, tpm):
+    screen.print_at('{}'.format(tpm.get_n_taps().rjust(3, ' ')), screen.width - 5, 0)
+    screen.print_at('TPM[{}/4]: {}     '.format(tpm.get_beats_per_bar(), tpm.get_tpm().rjust(10, ' ')), screen.width - 20, 1)
+
+
+def tpm_screen(screen, tpm):
+    while True:
+        update_tpm(screen, tpm)
+        screen.refresh()
+
+        key_code = get_key_blocking(screen)
+        if key_code == Screen.KEY_ESCAPE:
+            return
+        if key_code in (ord('1'), ord('2'), ord('3'), ord('4'), ord('5'), ord('6'), ord('7'), ord('8'), ord('9')):
+            tpm.set_beats_per_bar(key_code - ord('1') + 1)
+        else:
+            tpm.tap()
+
+
 
 
 def main_screen(screen):
@@ -54,7 +80,8 @@ def main_screen(screen):
             sp_song_title = items[items_offset]['name']
             sp_song_artist = items[items_offset]['artists'][0]['name']
             song_found = True
-        
+        tpm = init_tpm()
+
         goto_next_song = False
         while not goto_next_song:
             screen.clear()
@@ -65,19 +92,21 @@ def main_screen(screen):
             else:
                 screen.print_at('Spotify: no results', 0, 1)
 
-            screen.print_at('\r{:10}{:10}{:10}{:10}{:10}{:10}{:10} '.format('(q)uit', '(s)kip', '(p)lay', '(i) prev', '(o) next', '(u) pause', '(t)ap'), 0, 3)
-            screen.print_at('\r{:10}{:10}{:10}{:10} '.format('(j) artist -word', '(k) artist -char', '(l) title -word', '(m) title -char'), 0, 4)
+            update_tpm(screen, tpm)
+
+            screen.print_at('{:10}{:10}{:10}{:10}{:10}{:10}{:10}{:10} '.format('(a)dd', '(q)uit', '(s)kip', '(p)lay', '(i) prev', '(o) next', '(u) pause', '(t)ap'), 0, 3)
+            screen.print_at('{:10}{:10}{:10}{:10} '.format('(j) artist -word', '(k) artist -char', '(l) title -word', '(m) title -char'), 0, 4)
             
             screen.refresh()
             
-            ev = get_key_blocking(screen)
+            key_code = get_key_blocking(screen)
             
-            if ev in (ord('Q'), ord('q')):
+            if key_code in (ord('Q'), ord('q')):
                 return # quit
-            if ev in (ord('S'), ord('s')):
+            if key_code in (ord('S'), ord('s')):
                 goto_next_song = True
 
-            if ev in (ord('F'), ord('f')):
+            if key_code in (ord('F'), ord('f')):
                 items = sp_find(song_artist, song_title)
                 if items is not None:
                     n_items = len(items)
@@ -87,43 +116,43 @@ def main_screen(screen):
                     song_found = True
                 else:
                     song_found = False
-            if ev in (ord('P'), ord('p')):
+            if key_code in (ord('P'), ord('p')):
                 if song_found:
                     sp_play(items, items_offset)
-            if ev == ord('i'):
+            if key_code == ord('i'):
                 if items_offset != 0:
                     sp_skip(-1)
                     items_offset -= 1
-            if ev == ord('I'):
+            if key_code == ord('I'):
                 sp_seek(-1)
-            if ev == ord('o'):
+            if key_code == ord('o'):
                 if items_offset != n_items - 1:
                     sp_skip(1)
                     items_offset += 1
-            if ev == ord('O'):
+            if key_code == ord('O'):
                 sp_seek(1)
-            if ev in (ord('U'), ord('u')):
+            if key_code in (ord('U'), ord('u')):
                 sp_pause_resume()
 
-            if ev in (ord('T'), ord('t')):
-                pass
-                #tap_screen(some_arg)
+            if key_code in (ord('T'), ord('t')):
+                screen.print_at('TAPPING', screen.width - 20, 0)
+                tpm_screen(screen, tpm)
 
-            if ev == ord('j'):
+            if key_code == ord('j'):
                 song_artist.remove_word()
-            if ev == ord('k'):
+            if key_code == ord('k'):
                 song_artist.remove_char()
-            if ev == ord('l'):
+            if key_code == ord('l'):
                 song_title.remove_word()
-            if ev == ord('m'):
+            if key_code == ord('m'):
                 song_title.remove_char()
-            if ev == ord('J'):
+            if key_code == ord('J'):
                 song_artist.reveal_word()
-            if ev == ord('K'):
+            if key_code == ord('K'):
                 song_artist.reveal_char()
-            if ev == ord('L'):
+            if key_code == ord('L'):
                 song_title.reveal_word()
-            if ev in (ord('é'), ord('ö'), ord('Ö')):
+            if key_code in (ord('é'), ord('ö'), ord('Ö')):
                 song_title.reveal_char()
 
 sp_login()
@@ -132,23 +161,5 @@ Screen.wrapper(main_screen)
 if __name__ == '__main__':
     pass    
 
-
-
-
-""" for song in new_songs:
-    print()
-    print('-' * 100)
-    print()
-    print('[{}] - {} by {}'.format(song[2], song[1], song[0]))
-    
-    goto_next_song = False
-    while not goto_next_song:
-        print('\r{:10}{:10}{:10}{:10}{:10}{:10}{:10} '.format('(q)uit', '(s)kip', '(p)lay', '(i) prev', '(o) next', '(u) stop', '(t)ap'), end='')
-        choice = sys.stdin.readline()
-        if choice == 'q\n':
-            sys.exit()
-        if choice == 's\n':
-            goto_next_song = True
- """        
 
         
